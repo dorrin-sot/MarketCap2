c8c2d4ee12c374910a03a0186aa6aaacf55fc1fb
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-08-09 15:17:52 +0330

    Dependency clean-up

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -37,12 +37,13 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.appcompat)
   implementation(libs.material)
-  implementation(libs.retrofit)
-  implementation(libs.converter.gson)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
 
   implementation(libs.hilt.android)
   ksp(libs.hilt.compiler)
+
+  implementation(libs.retrofit)
+  implementation(libs.converter.gson)
 }
