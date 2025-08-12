1ecc913cc10ba3be1be4d63804594216dcb675aa
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-08-12 13:58:13 +0330

    Add Jetpack Navigation and implement BottomNavigationView in MainActivity
    This commit introduces Jetpack Navigation to the project and implements a `BottomNavigationView` in `MainActivity`.
    
    Key changes include:
    - Added Navigation Component dependencies (`navigation-fragment-ktx`, `navigation-ui-ktx`).
    - Updated `MainActivity` layout to use `FragmentContainerView` as a `NavHostFragment` and added a `BottomNavigationView`.
    - Configured `MainActivity` to set up the `BottomNavigationView` with the `NavController` and an `AppBarConfiguration`.
    - Implemented custom handling for `BottomNavigationView` item selection to pop the back stack and navigate to the selected destination.
    - Created a new navigation graph (`mobile_navigation.xml`) defining `ConversionFragment` and `CurrencyListFragment` as destinations.
    - Added a menu resource (`bottom_navigation_menu.xml`) for the `BottomNavigationView` items.
    - Included new drawable icons for the navigation items (`outline_conversion_path_24.xml`, `outline_list_24.xml`).
    - Added new string resources for navigation titles.

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
   implementation(libs.androidx.constraintlayout)
   implementation(libs.androidx.fragment.ktx)
   implementation(libs.rxandroid)
+  implementation(libs.androidx.navigation.fragment.ktx)
+  implementation(libs.androidx.navigation.ui.ktx)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
--- presentation/src/main/java/com/dorrin/presentation/MainActivity.kt
+++ presentation/src/main/java/com/dorrin/presentation/MainActivity.kt
@@ -5,26 +5,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.dorrin.presentation.currency_list.CurrencyListFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dorrin.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+  private var _binding: ActivityMainBinding? = null
+  private val binding get() = _binding!!
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContentView(R.layout.activity_main)
+
+    _binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
       insets
     }
 
-    if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction()
-        .add(R.id.main, CurrencyListFragment.newInstance())
-        .commit()
+    setupBottomNavigation()
+  }
+
+  private fun setupBottomNavigation() {
+    val navView = binding.bottomNav
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!
+    val navController = navHostFragment.findNavController()
+    val appBarConfiguration = AppBarConfiguration(
+      setOf(R.id.navigation_conversion, R.id.navigation_list)
+    )
+    setupActionBarWithNavController(navController, appBarConfiguration)
+    navView.setupWithNavController(navController)
+
+    navView.setOnItemSelectedListener { item ->
+      if (item.itemId != navView.selectedItemId) {
+        navController.popBackStack(item.itemId, inclusive = true, saveState = false)
+        navController.navigate(item.itemId)
+      }
+      true
     }
   }
 }
