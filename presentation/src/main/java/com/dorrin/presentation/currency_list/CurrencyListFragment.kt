ddedcb79237a7e0d8699367065862e9f3200c5d9
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-08-17 13:43:47 +0330

    Add search and search_off button to CurrencyListFragment toolbar

--- presentation/src/main/java/com/dorrin/presentation/currency_list/CurrencyListFragment.kt
+++ presentation/src/main/java/com/dorrin/presentation/currency_list/CurrencyListFragment.kt
@@ -2,21 +2,29 @@ package com.dorrin.presentation.currency_list
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dorrin.domain.entity.CurrencyEntity
+import com.dorrin.presentation.R
 import com.dorrin.presentation.databinding.FragmentCurrencyListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CurrencyListFragment : Fragment() {
+class CurrencyListFragment : Fragment(), MenuProvider {
   private var _binding: FragmentCurrencyListBinding? = null
   private val binding get() = _binding!!
+
+  private lateinit var menu: Menu
 
   private val viewModel by viewModels<CurrencyListViewModel>()
 
@@ -32,6 +40,7 @@ class CurrencyListFragment : Fragment() {
 
     binding.viewModel = viewModel
     binding.lifecycleOwner = viewLifecycleOwner
+    requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
     return binding.root
   }
@@ -39,6 +48,34 @@ class CurrencyListFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     viewModel.allCurrencies.observe(viewLifecycleOwner, AllCurrenciesObserver())
+  }
+
+  override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+    this.menu = menu
+    menuInflater.inflate(R.menu.currency_list_toolbar_menu, menu)
+    onMenuItemSelected(menu.findItem(R.id.toolbar_search_off)) // search off by default
+  }
+
+  override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+    when (menuItem.itemId) {
+      R.id.toolbar_search -> {
+        updateSearchBarVisibility(true)
+        true
+      }
+
+      R.id.toolbar_search_off -> {
+        updateSearchBarVisibility(false)
+        true
+      }
+
+      else -> false
+    }
+
+
+  private fun updateSearchBarVisibility(visible: Boolean) {
+    menu.findItem(R.id.toolbar_search).isVisible = !visible
+    menu.findItem(R.id.toolbar_search_off).isVisible = visible
+    // todo show and hide search bar
   }
 
   private inner class AllCurrenciesObserver : Observer<List<CurrencyEntity>> {
